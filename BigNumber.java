c07918afd35da0216f68958f85bccee316018df7
jithu837 <jithendrasolanki56@gmail.com>
2024-12-04 13:48:04 +0530

    Add files via upload

---
+++ BigNumber.java
@@ -0,0 +1,17 @@
+import java.util.*;
+ public class BigNumber
+ {
+    public static void main(String args[])
+    {
+        int a=65;
+        int b=110;
+        if((a>b)!==0)
+        {
+            System.out.println("a  is greater");
+
+        }else
+        {
+            System.out.println("b is greater"); 
+        }
+    } 
+ }
