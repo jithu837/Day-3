6ca36321c185ec7fc8c603f9fd88e083e448b50e
jithu837 <jithendrasolanki56@gmail.com>
2024-12-04 13:48:20 +0530

    Add files via upload

---
+++ Power1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ public class Power1
+ {
+    public static void main(String args[])
+    {
+        int a=36;
+        if((a&(a-2))==0)
+        {
+            System.out.println("Power of 4");
+
+        }else
+        {
+            System.out.println("Not Power of 4"); 
+        }
+    } 
+ }
