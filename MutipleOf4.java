ea5aa9f32097c5177fad541bf2efcfcb4c0d59f4
jithu837 <jithendrasolanki56@gmail.com>
2024-12-04 13:48:39 +0530

    Add files via upload

---
+++ MutipleOf4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+ public class MutipleOf4
+ {
+    public static void main(String args[])
+    {
+        int a=16;
+        if((a&3)==0)
+        {
+            System.out.println("Mutliple of 4");
+
+        }else
+        {
+            System.out.println("Not Multiple of 4"); 
+        }
+    } 
+ }
