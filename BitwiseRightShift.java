84cc7db9830429be51c53c2149f08d439abc9b56
jithu837 <jithendrasolanki56@gmail.com>
2024-12-05 10:04:11 +0530

    Add files via upload

---
+++ BitwiseRightShift.java
@@ -0,0 +1,12 @@
+public class BitwiseRightshift
+{
+    public static void main(String args[])
+    {
+        System.out.println(22>>2);
+        System.out.println(28>>3);
+        System.out.println(43>>4);
+        System.out.println(15>>1);
+        System.out.println(62>>6);
+        System.out.println(32>>7);
+    }
+}
